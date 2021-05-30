4e3e271f46ad472657155dc41d0b3baf0644faf0
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2021-05-30 11:52:16 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /etc/ssh/sshd_config "https://sc.vpnstores.net/Korlo/sshd.conf"
+wget -O /etc/ssh/sshd_config "https://raw.githubusercontent.com/Gugun09/root/main/sshd.conf"
 /etc/init.d/ssh restart
 passwd
